78b7ac432916193b1e799c3477faefec2b9d0595
Kenneth Hoff <kenneth.98@hotmail.com>
2022-05-31 23:40:29 +0200

    Update input-validation

--- input-validation/assets/scripts/index.ts
+++
@@ -1,5 +0,0 @@
-import {setupCrossInputValidation} from "./instances/crossInputValidation.js";
-import {setupCustomValidationPatterns} from "./instances/customValidationPatterns.js";
-
-setupCrossInputValidation();
-setupCustomValidationPatterns();
