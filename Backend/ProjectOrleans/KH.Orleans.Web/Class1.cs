0c3489d35de7d434cebcc6371c50b98c9a222eae
Kenneth Hoff <kenneth.98@hotmail.com>
2023-08-02 19:56:40 +0200

    Fix Analyzer Publishing

---
+++ Backend/ProjectOrleans/KH.Orleans.Web/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace KH.Orleans.Web;
+
+public class Class1
+{
+    public Class1(string firstName)
+    {
+        FirstName = firstName;
+    }
+
+    public string FirstName { get; set; }
+}
