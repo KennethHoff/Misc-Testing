bfb48a27dcc0ec1cf6f98e82930e00eb0a698971
Kenneth Hoff <kenneth.98@hotmail.com>
2023-08-02 23:00:44 +0200

    Streamline

--- Backend/Analyzers/Oxx.Backend.Analyzers/src/Oxx.Backend.Analyzers/Constants/AnalyzerId.cs
+++ Backend/Analyzers/Oxx.Backend.Analyzers/src/Oxx.Backend.Analyzers/Constants/AnalyzerId.cs
@@ -2,6 +2,10 @@ namespace Oxx.Backend.Analyzers.Constants;
 
 public static class AnalyzerId
 {
+	/// <summary>
+	/// Prefix for all analyzer IDs provided by this project.
+	/// </summary>
 	private const string Prefix = "OXX";
+
 	public const string RequiredProperty = Prefix + "0001";
 }
