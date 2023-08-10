22c99302689f4876ab1041c14c2eeb4726e804a4
Kenneth Hoff <kenneth.98@hotmail.com>
2023-08-10 18:00:51 +0200

    Remove suppresor

--- Backend/Analyzers/Oxx.Backend.Analyzers/src/Oxx.Backend.Analyzers.Sample/Examples.cs
+++ Backend/Analyzers/Oxx.Backend.Analyzers/src/Oxx.Backend.Analyzers.Sample/Examples.cs
@@ -1,7 +1,6 @@
 ﻿// ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
 namespace Oxx.Backend.Analyzers.Sample;
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 public class RequiredPropertyExamples
 {
@@ -9,5 +8,3 @@ public class RequiredPropertyExamples
 	public string LastName { get; set; }
 	public string Email { get; set; }
 }
-
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
