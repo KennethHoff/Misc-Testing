ab34b155b72b8e9a5ae5bea14266abc241493a89
kennethhoff <kenneth.98@hotmail.com>
2023-11-12 20:53:53 +0100

    remove meter name

--- Backend/KHtmx/src/KHtmx.Telemetry/MetricNames.cs
+++ Backend/KHtmx/src/KHtmx.Telemetry/MetricNames.cs
@@ -3,10 +3,6 @@ namespace KHtmx.Telemetry;
 public static class MetricNames
 {
     // TODO: Figure out what the point of the meter name is.
-    /// <summary>
-    /// Name of the meter for KHtmx
-    /// </summary>
-    public const string MeterName = "khtmx";
 
     private const string Prefix = "khtmx_";
     public const string CommentEditFailedValidation = Prefix + "comment_edit_failed_validation";
