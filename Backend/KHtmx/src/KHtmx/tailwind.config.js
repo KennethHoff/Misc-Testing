a9f5217d469c0ab60e0e551272841fe8e191f7a6
kennethhoff <kenneth.98@hotmail.com>
2023-11-12 14:41:26 +0100

    more style tweaks

--- Backend/KHtmx/src/KHtmx/tailwind.config.js
+++ Backend/KHtmx/src/KHtmx/tailwind.config.js
@@ -7,28 +7,44 @@ module.exports = {
         "./Components/**/*.razor",
         "wwwroot/**/*.js",
     ],
+    // TODO: Make the design more consistent
     theme: {
+        colors: {
+            transparent: 'transparent',
+            background: {
+                DEFAULT: colors.zinc[800],
+            },
+            primary: {
+                DEFAULT: colors.zinc[100],
+            },
+            action: {
+                DEFAULT: colors.blue[500],
+                active: colors.blue[400],
+            },
+            focus: {
+                DEFAULT: colors.zinc[100],
+            },
+            invalid: {
+                DEFAULT: colors.red[800],
+                active: colors.red[600],
+            },
+            valid: {
+                DEFAULT: colors.green[800],
+                active: colors.green[600],
+            },
+            // TODO: Get a better name for this concept
+            table: {
+                odd: colors.zinc[800],
+                even: colors.zinc[700],
+            },
+            hover: {
+                DEFAULT: colors.zinc[500],
+            }
+        },
         extend: {
             gridTemplateColumns: {
                 'comment-table': '125px 1fr 150px'
             },
-            colors: {
-                background: colors.zinc[800],
-                action: {
-                    DEFAULT: colors.zinc[600],
-                    active: colors.zinc[400],
-                },
-                focus: {
-                    DEFAULT: colors.zinc[100],
-                },
-                invalid: {
-                    DEFAULT: colors.red[800],
-                    active: colors.red[600],
-                },
-            },
-            // outlineStyle: {
-            //     dashed: 'dashed',
-            // }
         },
         outlineStyle: {
             dashed: 'dashed',
@@ -36,9 +52,9 @@ module.exports = {
     },
     plugins: [
         // Aria-invalid
-        function ({ addVariant, e }) {
-            addVariant('aria-invalid', ({ modifySelectors, separator }) => {
-                modifySelectors(({ className }) => {
+        function ({addVariant, e}) {
+            addVariant('aria-invalid', ({modifySelectors, separator}) => {
+                modifySelectors(({className}) => {
                     return `.${e(`aria-invalid${separator}${className}`)}[aria-invalid]`
                 })
             })
--- Backend/KHtmx/src/KHtmx/wwwroot/scripts/exception_toast.js
+++ Backend/KHtmx/src/KHtmx/wwwroot/scripts/exception_toast.js
@@ -5,47 +5,60 @@ htmx.on("htmx:afterRequest", (e) => {
     }
 });
 
+const dataRoles = {
+    toastContainer: "toast-container",
+    toast: "toast",
+    statusCode: "Status Code",
+    statusText: "Status Text",
+    responseText: "Response Text",
+    copyError: "Copy Error",
+    dismissError: "Dismiss Error",
+    toggleFullscreen: "Toggle Fullscreen"
+};
+
+const getDataRoleSelector = (role) => `[data-role='${role}']`;
+
 const detailsOpenFlag = "data-details-was-open";
 
 const toastContainerTemplate = `
-        <div data-role="toast-container" class="fixed bottom-0 right-0 p-4 flex flex-col gap-4"></div>
-    `;
+    <div data-role="${dataRoles.toastContainer}" class="fixed bottom-0 right-0 p-4 flex flex-col gap-4"></div>
+`;
 
 const detailsTemplate = `
-        <details data-role="toast" class="bg-red-700 text-white font-bold rounded-2xl px-4 py-2 cursor-pointer max-w-screen-lg">
-            <summary class="flex flex-row gap-20 p-4 place-content-between place-items-center">
-                <span>
-                    <span data-role="Status Code">{Status}</span>
-                    <span data-role="Status Text">{StatusText}</span>
-                </span>
-                <div class="flex flex-row gap-8 place-content-center">
-                    <button data-role="Copy Error" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Copy</button>
-                    <button data-role="Make Fullscreen" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Fullscreen</button>
-                    <button data-role="Dismiss Error" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Dismiss</button>
-                </div>
-            </summary>
-            <pre data-role="Response Text" class="border border-t-0 border-red-400 rounded-b bg-red-800 p-4 text-red-200 cursor-auto overflow-y-auto max-h-96">{ResponseText}</pre>
-        </details>
-    `;
+    <details data-role="${dataRoles.toast}" class="bg-invalid rounded-2xl px-4 py-2 cursor-pointer max-w-screen-lg">
+        <summary class="flex flex-row gap-20 p-4 place-content-between place-items-center font-bold">
+            <span>
+                <span data-role="${dataRoles.statusCode}">{Status}</span>
+                <span data-role="${dataRoles.statusText}">{StatusText}</span>
+            </span>
+            <div class="flex flex-row gap-8 place-content-center">
+                <button data-role="${dataRoles.copyError}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Copy</button>
+                <button data-role="${dataRoles.toggleFullscreen}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Fullscreen</button>
+                <button data-role="${dataRoles.dismissError}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Dismiss</button>
+            </div>
+        </summary>
+        <pre data-role="${dataRoles.responseText}" class="border-2 border-primary rounded-b bg-invalid p-4 text-primary cursor-auto overflow-y-auto max-h-96 font-code">{ResponseText}</pre>
+    </details>
+`;
 
 const dialogTemplate = `
-        <dialog data-role="toast" class="p-4 rounded-xl bg-red-700 max-w-[80vw] max-h-[80vh]">
-            <header class="flex flex-row gap-20 p-4 place-content-between place-items-center">
-                <span>
-                    <span data-role="Status Code">{Status}</span>
-                    <span data-role="Status Text">{StatusText}</span>
-                </span>
-                <div class="flex flex-row gap-8 place-content-center">
-                    <button data-role="Copy Error" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Copy</button>
-                    <button data-role="Make Fullscreen" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Fullscreen</button>
-                    <button data-role="Dismiss Error" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Dismiss</button>
-                </div>
-            </header>
-            <main>
-                <pre data-role="Response Text" class="border border-t-0 border-gray-400 rounded-b bg-red-800 p-4 text-gray-200 cursor-auto overflow-scroll">{ResponseText}</pre>
-            </main>
-        </dialog>
-    `;
+    <dialog data-role="${dataRoles.toast}" class="p-4 rounded-xl bg-invalid max-w-[80vw] max-h-[80vh]">
+        <header class="flex flex-row gap-20 p-4 place-content-between place-items-center font-bold">
+            <span>
+                <span data-role="${dataRoles.statusCode}">{Status}</span>
+                <span data-role="${dataRoles.statusText}">{StatusText}</span>
+            </span>
+            <div class="flex flex-row gap-8 place-content-center">
+                <button data-role="${dataRoles.copyError}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Copy</button>
+                <button data-role="${dataRoles.toggleFullscreen}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Minimize</button>
+                <button data-role="${dataRoles.dismissError}" class="font-bold py-2 px-4 rounded bg-action hover:bg-action-active focus:bg-action-active">Dismiss</button>
+            </div>
+        </header>
+        <main>
+            <pre data-role="${dataRoles.responseText}" class="border-2 border-primary rounded-b bg-invalid p-4 text-primary cursor-auto overflow-y-auto max-h-96 font-code">{ResponseText}</pre>
+        </main>
+    </dialog>
+`;
 
 function createErrorToast(e) {
     const toastContainer = ensureToastContainerExists();
@@ -61,25 +74,25 @@ function createErrorToast(e) {
 
     addClickListeners(
         toastContainer,
-        detailsElement.querySelector("[data-role='Copy Error']"),
-        detailsElement.querySelector("[data-role='Dismiss Error']"),
-        detailsElement.querySelector("[data-role='Make Fullscreen']"),
+        detailsElement.querySelector(getDataRoleSelector(dataRoles.copyError)),
+        detailsElement.querySelector(getDataRoleSelector(dataRoles.dismissError)),
+        detailsElement.querySelector(getDataRoleSelector(dataRoles.toggleFullscreen)),
         detailsElement
     );
 }
 
 function addClickListeners(toastContainer, copyBtn, closeBtn, fullscreenBtn, toastElement) {
-    copyBtn.addEventListener("click", (e) => {
-        const text = e.target.closest("[data-role='toast']").querySelector("pre").innerHTML;
-        navigator.clipboard.writeText(text);
+    copyBtn.addEventListener("click", async (e) => {
+        const text = e.target.closest(getDataRoleSelector(dataRoles.toast)).querySelector("pre").innerHTML;
+        await navigator.clipboard.writeText(text);
     });
 
     closeBtn.addEventListener("click", (e) => {
-        e.target.closest("[data-role='toast']").remove();
+        e.target.closest(getDataRoleSelector(dataRoles.toast)).remove();
     });
 
     fullscreenBtn.addEventListener("click", (e) => {
-        const wrapperElement = e.target.closest("[data-role='toast']");
+        const wrapperElement = e.target.closest(getDataRoleSelector(dataRoles.toast));
         if (wrapperElement.tagName === "DETAILS") {
             convertToDialog(toastContainer, wrapperElement);
         } else {
@@ -102,9 +115,9 @@ function addClickListeners(toastContainer, copyBtn, closeBtn, fullscreenBtn, toa
 function convertToDialog(toastContainer, existingDetailsElement) {
     // Get values from existing details element
     const values = {
-        Status: existingDetailsElement.querySelector("[data-role='Status Code']").innerHTML,
-        StatusText: existingDetailsElement.querySelector("[data-role='Status Text']").innerHTML,
-        ResponseText: existingDetailsElement.querySelector("[data-role='Response Text']").innerHTML
+        Status: existingDetailsElement.querySelector(getDataRoleSelector(dataRoles.statusCode)).innerHTML,
+        StatusText: existingDetailsElement.querySelector(getDataRoleSelector(dataRoles.statusText)).innerHTML,
+        ResponseText: existingDetailsElement.querySelector(getDataRoleSelector(dataRoles.responseText)).innerHTML
     };
 
     const wasOpen = existingDetailsElement.hasAttribute("open");
@@ -122,9 +135,9 @@ function convertToDialog(toastContainer, existingDetailsElement) {
     }
 
     // Add event listeners to new elements
-    const newCopyButtonElement = newDialogElement.querySelector("[data-role='Copy Error']");
-    const newCloseButtonElement = newDialogElement.querySelector("[data-role='Dismiss Error']");
-    const newFullscreenButtonElement = newDialogElement.querySelector("[data-role='Make Fullscreen']");
+    const newCopyButtonElement = newDialogElement.querySelector(getDataRoleSelector(dataRoles.copyError));
+    const newCloseButtonElement = newDialogElement.querySelector(getDataRoleSelector(dataRoles.dismissError));
+    const newFullscreenButtonElement = newDialogElement.querySelector(getDataRoleSelector(dataRoles.toggleFullscreen));
 
     addClickListeners(
         toastContainer,
@@ -141,9 +154,9 @@ function convertToDialog(toastContainer, existingDetailsElement) {
 function convertToDetails(toastContainer, existingDialogElement) {
     // Get values from existing dialog element
     const values = {
-        Status: existingDialogElement.querySelector("[data-role='Status Code']").innerHTML,
-        StatusText: existingDialogElement.querySelector("[data-role='Status Text']").innerHTML,
-        ResponseText: existingDialogElement.querySelector("[data-role='Response Text']").innerHTML
+        Status: existingDialogElement.querySelector(getDataRoleSelector(dataRoles.statusCode)).innerHTML,
+        StatusText: existingDialogElement.querySelector(getDataRoleSelector(dataRoles.statusText)).innerHTML,
+        ResponseText: existingDialogElement.querySelector(getDataRoleSelector(dataRoles.responseText)).innerHTML
     };
 
     // Create new elements
@@ -156,9 +169,9 @@ function convertToDetails(toastContainer, existingDialogElement) {
     existingDialogElement.replaceWith(newDetailsElement);
 
     // Add event listeners to new elements
-    const newCopyButtonElement = newDetailsElement.querySelector("[data-role='Copy Error']");
-    const newCloseButtonElement = newDetailsElement.querySelector("[data-role='Dismiss Error']");
-    const newFullscreenButtonElement = newDetailsElement.querySelector("[data-role='Make Fullscreen']");
+    const newCopyButtonElement = newDetailsElement.querySelector(getDataRoleSelector(dataRoles.copyError));
+    const newCloseButtonElement = newDetailsElement.querySelector(getDataRoleSelector(dataRoles.dismissError));
+    const newFullscreenButtonElement = newDetailsElement.querySelector(getDataRoleSelector(dataRoles.toggleFullscreen));
 
     // Open the details
     if (existingDialogElement.hasAttribute(detailsOpenFlag)) {
@@ -175,14 +188,14 @@ function convertToDetails(toastContainer, existingDialogElement) {
 }
 
 function ensureToastContainerExists() {
-    const existingToastContainer = document.querySelector("[data-role='toast-container']");
+    const existingToastContainer = document.querySelector(getDataRoleSelector(dataRoles.toastContainer));
     if (existingToastContainer) {
         return existingToastContainer;
     }
 
     const templateElement = document.createElement("template");
     templateElement.innerHTML = toastContainerTemplate;
-    const newToastContainer = templateElement.content.querySelector("[data-role='toast-container']");
+    const newToastContainer = templateElement.content.querySelector(getDataRoleSelector(dataRoles.toastContainer));
     document.body.appendChild(newToastContainer);
 
     // add mutation observer to delete the container if it's empty.
