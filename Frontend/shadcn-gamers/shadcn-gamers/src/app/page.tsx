9a90a77412c11879b17437ebbe70f5edd4679239
kennethhoff <kenneth.98@hotmail.com>
2024-03-06 19:37:57 +0100

    Add dummy text

--- Frontend/shadcn-gamers/shadcn-gamers/src/app/page.tsx
+++ Frontend/shadcn-gamers/shadcn-gamers/src/app/page.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<Carousel className="w-full max-w-xs">
+			<CarouselContent>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<CarouselItem key={index}>
+						<div className="p-1">
+							Dette er tekst #{index + 1}
+							{/* <Card> */}
+							{/* 	<CardContent className="flex aspect-square items-center justify-center p-6"> */}
+							{/* 		<span className="text-4xl font-semibold">{index + 1}</span> */}
+							{/* 	</CardContent> */}
+							{/* </Card> */}
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
 		
     </main>
   );
