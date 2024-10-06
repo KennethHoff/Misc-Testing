8dd81a6399b6db1398a806df2c455dd232a6d243
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-06 18:02:37 +0200

    huh

--- FSharp/hello-world.fsx
+++ FSharp/hello-world.fsx
@@ -1,8 +1,15 @@
+type ThingyMaJigg = Red of string | Blue of int
+
 // Define a function that takes an integer and returns its square
-let square x = x * x
+let square x =
+  match x with
+  | Red red -> red
+  | Blue blue -> (blue * blue).ToString();
+
+let thingy = 5;
 
 // Print the square of 5
-printfn "The square of 5 is: %d" (square 5)
+printfn "The square of %d is: %s" thingy (square (Blue thingy))
 
 // A simple loop that prints numbers from 1 to 5
 for i in 1..5 do
