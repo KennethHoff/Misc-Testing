c7615db52a3aac1df01bb41c698f5001c5069da7
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-05 16:27:44 +0100

    [Zig] Create flake.nix

---
+++ Zig/flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Ensure latest pkgs
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.zig
+          ];
+        };
+      }
+    );
+}
