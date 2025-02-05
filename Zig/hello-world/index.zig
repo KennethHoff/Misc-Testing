ab61b7a0c3510cb42b4a24806e67a1d3d868b904
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-05 17:07:01 +0100

    [Zig] Make it even more cursed

--- Zig/hello-world/index.zig
+++ Zig/hello-world/index.zig
@@ -1,13 +1,18 @@
 const std = @import("std");
+const bufferSize = 32;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
 
-    var buffer = [_]u8{0} ** 32;
+    var buffer = [_]u8{0} ** (bufferSize + 1);
 
     const bytes_read = try stdin.readAll(&buffer);
+    if (bytes_read > bufferSize) {
+        try stdout.print("Too long, bro. The maxiumum is {d}\n", .{bufferSize});
+        return;
+    }
     const input_str = buffer[0 .. bytes_read - 1];
 
-    try stdout.print("Hello, {s}!\n", .{input_str});
+    try stdout.print("Hello, '{s}'!\n", .{input_str});
 }
