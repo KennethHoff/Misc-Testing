65acd8c8a2421d7d5a90b0cbfb672cf4446f584c
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-05 17:04:34 +0100

    [Zig] Create a cursed stdin parser

--- Zig/hello-world/index.zig
+++ Zig/hello-world/index.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("{s} {s}\n", .{ "Hello", "World" });
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
+    var buffer = [_]u8{0} ** 32;
+
+    const bytes_read = try stdin.readAll(&buffer);
+    const input_str = buffer[0 .. bytes_read - 1];
+
+    try stdout.print("Hello, {s}!\n", .{input_str});
 }
