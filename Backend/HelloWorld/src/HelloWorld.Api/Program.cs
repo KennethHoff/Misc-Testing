ad9a6e86a9ca4cab6158f3b9cdcf3ead002775a3
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-06 20:06:46 +0100

    [Backend/Helloworld] Create a simple Web Api

---
+++ Backend/HelloWorld/src/HelloWorld.Api/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+}
+
+app.MapGet("Test", async (HttpRequest httpRequest) =>
+{
+	await Task.Delay(1000);
+	return "test";
+});
+
+app.Run();
