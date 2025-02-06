78bd4891a9e35853bc485d5a330cb56000fd5aa7
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-06 19:54:59 +0100

    [Backend/Helloworld] Create Project w/ Flake

---
+++ Backend/HelloWorld/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Ensure latest pkgs
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (
+              with pkgs.dotnetCorePackages;
+              combinePackages [
+                sdk_9_0
+                sdk_8_0
+              ]
+            )
+          ];
+        };
+      }
+    );
+}
